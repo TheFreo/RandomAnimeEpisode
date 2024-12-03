0f415239c241f0f7ef9b629db73ce7e9a2b1e725
TheFreo <st.surgut05@mail.ru>
2024-12-03 18:56:32 +0700

    Anime information redone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import ReactPlayer from 'react-player';
+import libriaIco from './libriaIco.svg';
 
 function App() {
+  const cacheUrl = 'https://cache.libria.fun';
+  const libriaUrl = 'https://anilibria.tv';
   const [animeData, setAnimeData] = useState(null);
   const [episodeNum, setEpisodeNum] = useState(null);
-  const cacheVideo = 'https://cache.libria.fun';
-  const cachePoster = 'https://anilibria.tv';
+  const [episodeUrl, setEpisodeUrl] = useState(null);
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,39 +19,52 @@ function App() {
         setAnimeData(data);
 
         const randEpisode = Math.floor(Math.random() * data.player.episodes.last);
-        const selectedVideo = `${cacheVideo}${data.player.list[randEpisode].hls.hd}`;
         setEpisodeNum(randEpisode)
 
-        localStorage.setItem('url2Watch', selectedVideo);
+        const selectedVideo = `${cacheUrl}${data.player.list[randEpisode].hls.hd}`;
+        setEpisodeUrl(selectedVideo);
+
       } catch (error) {
-        console.error('Ошибка при загрузке данных:', error);
+        console.error(error);
       }
     };
-
     fetchData();
   }, []);
 
   if (!animeData) {
-    return <div className='load'>Загрузка...</div>; // Показываем индикатор загрузки
+    return <div className='load'>Загрузка...</div>;
   }
 
   const handleVideoEnd = () => {
     window.location.reload();
   };
 
+  const handlePlay = () => {
+    setIsActive(false)
+  };
+
+  const handlePause = () => {
+    setIsActive(true)
+  };
+
   return (
     <>
-      <section>
-        <div className="player">
-          <ReactPlayer width='100%' height='100vh' playing={true} onEnded={handleVideoEnd} controls={true} url={localStorage.getItem('url2Watch')} />
+      <section className={isActive ? 'desc hidden' : 'desc'}>
+        <div className='desc__inner'>
+          <img src={`${libriaUrl}${animeData.posters.small.url}`} alt="Logo" />
+          <div>
+            <h1>{animeData.names.en}</h1>
+            <h1 className='altName'>{animeData.names.ru}</h1>
+            <h2>Эпизод: {episodeNum}</h2>
+            <a target="_blank" href={`${libriaUrl}/release/${animeData.code}.html`}>
+              <img src={libriaIco} />
+            </a>
+          </div>
         </div>
       </section>
-      <section className='desc'>
-        <img src={`${cachePoster}${animeData.posters.medium.url}`} alt="Logo" />
-        <div>
-          <h1>{animeData.names.en}</h1>
-          <h1 className='altName'>{animeData.names.ru}</h1>
-          <h2>Episode: {episodeNum}</h2>
+      <section>
+        <div className="player">
+          <ReactPlayer class='video' width='100%' height='100vh' playing={true} onPlay={handlePlay} onPause={handlePause} onEnded={handleVideoEnd} controls={true} url={episodeUrl} />
         </div>
       </section>
     </>
